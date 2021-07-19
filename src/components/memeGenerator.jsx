856a06b3ee3e153d9a7bbf4121f53556c0637f6d
Lena Namiki <lena.namiki@gmail.com>
2021-07-19 18:34:43 +0900

    Created function to return random meme image on form submit

--- src/components/memeGenerator.jsx
+++ src/components/memeGenerator.jsx
@@ -10,6 +10,8 @@ class MemeGenerator extends Component {
       image: 'http://i.imgflip.com/1bij.jpg',
       allMemeImgs: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -23,20 +25,31 @@ class MemeGenerator extends Component {
 
   handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { allMemeImgs } = this.state;
+    const randNum = Math.floor(Math.random() * allMemeImgs.length);
+    const randMeme = allMemeImgs[randNum].url;
+    this.setState({ image: randMeme });
   };
 
   render() {
     const { topText, bottomText, image } = this.state;
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type="text" name="topText" value={topText} placeholder="Enter Text" onChange={this.handleChange} />
           <input type="text" name="bottomText" value={bottomText} placeholder="Enter Text" onChange={this.handleChange} />
           <button type="submit">GENERATE</button>
         </form>
+        <div>
+          <img src={image} alt="meme" />
+          <h2>{topText}</h2>
+          <h2>{bottomText}</h2>
+        </div>
       </div>
     );
   }
