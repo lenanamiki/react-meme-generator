45dae79a00566706064f51d3eb8a03a3c076d4d7
Lena Namiki <lena.namiki@gmail.com>
2021-07-19 14:25:32 +0900

    Created basic template for header and memegenerator component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/header';
+import MemeGenerator from './components/memeGenerator';
 
 function App() {
   return (
-    <h1>Hello!</h1>
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
   );
 }
 
---
+++ src/components/memeGenerator.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <p>This is the generator component</p>
+    );
+  }
+}
+
+export default MemeGenerator;
