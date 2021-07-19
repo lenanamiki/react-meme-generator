1f711a5ddf2aaa7a845324c468793a6e8e832c8d
Lena Namiki <lena.namiki@gmail.com>
2021-07-19 16:28:03 +0900

    API call from imglip

--- src/components/memeGenerator.jsx
+++ src/components/memeGenerator.jsx
@@ -7,8 +7,19 @@ class MemeGenerator extends Component {
     this.state = {
       topText: 'top text',
       bottomText: 'bottom text',
-      image: 'http://i.imgflip.com/1bij.jpg'
+      image: 'http://i.imgflip.com/1bij.jpg',
+      allMemeImgs: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(response => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        console.log(memes[0]);
+        this.setState({ allMemeImgs: memes });
+      });
   }
 
   render() {
@@ -17,7 +28,7 @@ class MemeGenerator extends Component {
       <div>
         <p>{topText}</p>
         <p>{bottomText}</p>
-        <img src="{image}" alt="meme" />
+        <img src={image} alt="meme" />
       </div>
     );
   }
